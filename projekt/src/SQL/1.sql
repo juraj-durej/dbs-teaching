a494fdc445f8b595a5c564c420532959854db722
Juraj Ďurej <jdurej@Jurajs-MacBook-Pro.local>
2022-02-23 23:45:26 +0100

    2. commit

--- projekt/src/SQL/1.sql
+++ projekt/src/SQL/1.sql
@@ -1,5 +1,31 @@
 select * from film;
 
+-- DB najprv musi niekde byt hostovana (lokalne v PC alebo na vzdialenom servery)
+
+-- nasledne je potrebne sa do nej prihlasit zadanim (url, name, password, database_name)
+
+-- nasledne je potrebne vytvorit databazu (databazovy server moze mat viacero databaz)
+
+CREATE DATABASE my_database_name
+WITH
+   [OWNER =  role_name]
+   [ENCODING = encoding]
+   [LC_COLLATE = collate]
+   [ALLOW_CONNECTIONS = true | false]
+   [CONNECTION LIMIT = max_concurrent_connection]
+
+
+-- colate/encoding
+
+-- In database systems, Collation specifies how data is sorted and compared in a 
+-- database. Collation provides the sorting rules, case, and accent sensitivity properties 
+-- for the data in the database.
+
+-- For example, when you run a query using the ORDER BY clause, collation determines whether 
+-- or not uppercase letters and lowercase letters are treated the same.
+
+-- vytvorenie tabulky v databaze
+
 create table film
 (
     film_id          integer default nextval('film_film_id_seq'::regclass) not null -- sekvencia generovania ID
